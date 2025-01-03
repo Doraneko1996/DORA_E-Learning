f0e1a48ac8193e0837b4a237a33eefe803cac314
Doraneko <kurosawa.dora96@gmail.com>
2025-01-04 01:23:49 +0700

    Update Sidebar Menu: Đóng/Mở, lưu state bằng pinna
    Đóng/Mở, lưu state bằng pinna
    Đổi tên component thành SidebarMenu

---
+++ frontend_vue/src/components/SidebarMenuToggle.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useLayoutStore } from '@/stores/layout'
+import Button from 'primevue/button'
+
+const layoutStore = useLayoutStore()
+</script>
+
+<template>
+    <Button @click="layoutStore.toggleMenu"
+        :icon="layoutStore.isMenuCollapsed ? 'pi pi-caret-right' : 'pi pi-caret-left'"
+        class="bg-surface-0 dark:bg-surface-900" variant="text" raised aria-label="Sidebar Menu" />
+</template>
